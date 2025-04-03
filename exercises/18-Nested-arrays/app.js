fff311917e9840297c3b967be860dccb158e0316
Dabeldab <1dareios@gmail.com>
2025-04-03 00:41:26 +0000

    lesson18

--- exercises/18-Nested-arrays/app.js
+++ exercises/18-Nested-arrays/app.js
@@ -1,3 +1,6 @@
 let coordinatesArray = [[33.747252, -112.633853], [-33.867886, -63.987], [41.303921, -81.901693], [-33.350534, -71.653268]];
 
 // Your code here
+    coordinatesArray.forEach(coordinatesArray => {
+    console.log(coordinatesArray[0][1],coordinatesArray[1][1],coordinatesArray[2][1],coordinatesArray[3][1])
+}) 
